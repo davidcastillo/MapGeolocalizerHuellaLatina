88acac46ed0b94e20bef8c06a38939ef4cc736f4
David Castillo <castillorganization@gmail.com>
2021-07-21 22:52:33 +0200

    initializing left panel

---
+++ src/components/BottonSection.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const BottonSection = () => {
+    return (
+        <div>
+            I am the Botton section
+        </div>
+    )
+}
+
+export default BottonSection
