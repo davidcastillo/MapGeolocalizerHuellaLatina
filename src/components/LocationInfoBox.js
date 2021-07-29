f64d578f50cd3b96b61ab1153fe08a6d95e166a8
David Castillo <castillorganization@gmail.com>
2021-07-29 20:08:37 +0200

    adjusting css

--- src/components/LocationInfoBox.js
+++ src/components/LocationInfoBox.js
@@ -2,16 +2,23 @@ import Logo from "./Logo";
 import BottonSection from "./BottonSection";
 
 const LocationInfoBox = ({info}) => {
+    function hideInfoBox(e){
+        e.preventDefault();
+        console.log("Hello David");
+    }
+
+
     return (
         
         <div className="location-info">
-            <span className="closeInfoBox" >X</span>
+            <span className="closeInfoBox" onClick={hideInfoBox} >X</span>
             <Logo />
             <h2>{info.user}</h2> 
             <div className="asociate-info">
                 <ul>
-                    <li><strong>Country of Origin:</strong> {info.nationality}             <img className="flag"  src={"https://www.countryflags.io/"+info.nationality.split('-').pop()+"/flat/32.png"}/>
-</li>
+                    <li><strong>Country of Origin:</strong> {info.nationality}
+                        <img className="flag"  src={"https://www.countryflags.io/"+info.nationality.split('-').pop()+"/flat/32.png"}/>
+                    </li>
                     <li><strong>Location:</strong> {info.location}</li>
                     <li><strong>Status: </strong> {info.status}</li>
                 </ul>
